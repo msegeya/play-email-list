fd505ed65bf34e78b34b065f1d8571196b231407
Logan Esch <lesch@sofi.org>
2016-01-15 10:37:33 -0700

    add email validation and hide constraints displaying on fields.

--- app/forms/AddressForm.java
+++ app/forms/AddressForm.java
@@ -1,10 +1,10 @@
 package forms;
 
-import play.data.validation.Constraints.MaxLength;
-import play.data.validation.Constraints.MinLength;
+import play.data.validation.Constraints.Email;
 import play.data.validation.Constraints.Required;
 
 public class AddressForm {
+    @play.data.validation.Constraints.Email
     @play.data.validation.Constraints.Required
     private String address;
 
