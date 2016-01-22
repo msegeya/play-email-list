a93d8c25445422d71359821ca4570155b0d5913a
Logan Esch <lesch@sofi.org>
2016-01-22 16:52:14 -0700

    update AddressForm docstring.

--- app/forms/AddressForm.java
+++ app/forms/AddressForm.java
@@ -7,9 +7,11 @@ import javax.persistence.Column;
 
 /**
  * Form to ask user for an email address.
+ * <p>
+ *     address must pass play's Email and Required validations. 
+ * </p>
  */
 public class AddressForm {
-    @Column(unique = true)
     // validate that input is an email.
     @Email
     // validate that the input is non-empty.
