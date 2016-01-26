86f6bee75d1568a02da0e7e96c489b25f00db104
Logan Esch <lesch@sofi.org>
2016-01-26 13:35:35 -0700

    add tests for email validation rules.

--- test/controllers/ApplicationTest.java
+++ test/controllers/ApplicationTest.java
@@ -1,7 +1,10 @@
 package controllers;
 
+import configs.AppConfig;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
@@ -16,7 +19,13 @@ import static play.test.Helpers.*;
  * Simple (JUnit) tests that can call all parts of a play app. If you are interested in mocking a whole application, see the wiki
  * for more details.
  */
+@ContextConfiguration(classes = {
+                AppConfig.class, config.TestDataConfig.class
+})
 public class ApplicationTest extends WithApplication {
+
+    @Autowired
+    Application controller;
 
     @Before
     public void setUp() throws Exception {
@@ -56,7 +65,7 @@ public class ApplicationTest extends WithApplication {
     }
 
     @Test
-    public void testAddAddressNotAddress() {
+    public void testAddAddressIllegalCharacters() {
         HashMap<String, String> map = new HashMap<>();
         map.put("Address", "blah<>!@#$&%*^()'");
 
@@ -69,4 +78,65 @@ public class ApplicationTest extends WithApplication {
             }
         });
     }
+
+    @Test
+    public void testAddAddressLocal64(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Address", "local64aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@a.com");
+
+        running(fakeApplication(), new Runnable() {
+            @Override
+            public void run() {
+                Result result = callAction(controllers.routes.ref.Application.addAddress(), fakeRequest().withFormUrlEncodedBody(map));
+                assertThat(status(result)).isEqualTo(BAD_REQUEST);
+                assertThat(contentAsString(result)).contains("Address already exists in the list.");
+            }
+        });
+    }
+
+    @Test
+    public void testAddAddressOAL254(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Address", "oal254@aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.com");
+
+        running(fakeApplication(), new Runnable() {
+            @Override
+            public void run() {
+                Result result = callAction(controllers.routes.ref.Application.addAddress(), fakeRequest().withFormUrlEncodedBody(map));
+                assertThat(status(result)).isEqualTo(BAD_REQUEST);
+                assertThat(contentAsString(result)).contains("Address already exists in the list.");
+            }
+        });
+    }
+
+    @Test
+    public void testAddAddressLocal65(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Address", "local65aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@a.com");
+
+        running(fakeApplication(), new Runnable() {
+            @Override
+            public void run() {
+                Result result = callAction(controllers.routes.ref.Application.addAddress(), fakeRequest().withFormUrlEncodedBody(map));
+                assertThat(status(result)).isEqualTo(BAD_REQUEST);
+                assertThat(contentAsString(result)).contains("Max length is 64 characters.");
+            }
+        });
+    }
+
+    @Test
+    public void testAddAddressOAL255(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Address", "oal255@aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.com");
+
+        running(fakeApplication(), new Runnable() {
+            @Override
+            public void run() {
+                Result result = callAction(controllers.routes.ref.Application.addAddress(), fakeRequest().withFormUrlEncodedBody(map));
+                assertThat(status(result)).isEqualTo(BAD_REQUEST);
+                assertThat(contentAsString(result)).contains("Max length is 254 characters.");
+            }
+        });
+    }
+
 }
