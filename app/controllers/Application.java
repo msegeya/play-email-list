284f3a2d2abb6206451aff3908988449bcf5e7aa
Logan Esch <lesch@sofi.org>
2016-01-26 16:55:34 -0700

    move bulk of validation to form.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,7 +13,6 @@ import play.mvc.Result;
 import services.AddressService;
 import services.TLDService;
 import views.html.index;
-
 import java.util.List;
 
 /**
@@ -28,8 +27,7 @@ import java.util.List;
 public class Application extends Controller {
 
     private final static Logger log = LoggerFactory.getLogger(Application.class);
-
-    private List<TLD> tlds;
+    private final List<TLD> tlds;
 
     // use injection to set up the addressService for talking to the database.
     @Autowired
@@ -69,19 +67,17 @@ public class Application extends Controller {
             log.debug("Bad input.");
             return badRequest(index.render(form, listAddress()));
         }
+        log.debug("Got new input: '{}'", form.get().getAddress());
 
-        // create a new address object to contain the information from the form.
-        Address address = new Address();
-        address.setAddress(form.get().getAddress());
-        log.info("Got new input: '{}'", address.getAddress());
-
-        // validate the address has a TLD.
-        String msg = validate(address.getAddress());
-        if (msg != null) {
-            //address does not have a TLD, return error.
+        String msg = validateTLD(form.get().getAddress());
+        if(msg != null){
+            log.debug("Bad TLD.");
             form.reject("address", msg);
             return badRequest(index.render(form, listAddress()));
         }
+        // create a new address object to contain the information from the form.
+        Address address = new Address();
+        address.setAddress(form.get().getAddress());
 
         // try to add the address to the DB.
         // will return false if address was in DB, in which case we should inform the user.
@@ -108,66 +104,9 @@ public class Application extends Controller {
         return redirect(routes.Application.index());
     }
 
-    /**
-     * Builds a list of the addresses in the DB. Used to display addresses in DB to user in index generation.
-     */
-    private List<Address> listAddress() {
-        // Get the addresses from the DB.
-        List<Address> al = addressService.getAllAddresses();
-        log.info("Returning list of stored addresses. Length: {}", al.size());
-        return al;
-    }
-
-    /**
-     * Validate that a {@link String} is an email address. Used in conjunction with @Required
-     * validation on {@link AddressForm}.
-     * <p>
-     * Needs {@link Application#tlds} to be set for the class and contain a list of valid {@link TLD}s.
-     * This can be set using {@link TLDService#getAllTLDs()}.
-     * <p>
-     * Checks that string passes initial regex (looks roughly like an email).
-     * Checks that the overall length is less than 255.
-     * Checks that the local length is less than 65.
-     * Requires that the String does not contain '..' then splits string on periods.
-     * Requires that result of split is at least length 2.
-     * Then requires that the last substring from the split does not contain '@'.
-     * Finally compares that substring against list of {@link TLD}s. If a match is found the string is accepted.
-     *
-     * @param address the String to validate.
-     * @return Null if String is an email, or message detailing why String is not an email.
-     */
-    private String validate(String address) {
-        log.debug("Validating TLD.");
-
-        // verify address meets regex, stolen from Play's email validation, but removed length constraints since they were not helpful
-        // and are enforced below.
-        final java.util.regex.Pattern regex = java.util.regex.Pattern.compile("\\b[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)*\\b");
-        if(!regex.matcher(address).matches()){
-            return Play.application().configuration().getString("msg.addressNotMatch");
-        }
-
-        //split the address on the @
-        String[] splitAddress = address.split("@");
-
-        // check the overall length of the address.
-        if(address.length() > 254){
-            log.debug("Overall address is too long. Length: {}", address.length());
-            return String.format(Play.application().configuration().getString("msg.addressTooLongOverall"), address.length());
-        }
-
-        //check 'local' part for length.
-        if(splitAddress[0].length() > 64){
-            log.debug("Local part of address is too long. Length: {}", splitAddress[0].length());
-            return String.format(Play.application().configuration().getString("msg.addressTooLongLocal"), splitAddress[0], splitAddress[0].length());
-        }
-
-        // Check for double period.
-        if (address.contains("..")) {
-            return Play.application().configuration().getString("msg.doublePeriod");
-        }
-
+    private String validateTLD(String address){
         // try and grab from the '.' to the end
-        splitAddress = address.split("\\.");
+        String[] splitAddress = address.split("\\.");
         if (splitAddress.length <= 1) {
             // if the array length is 1, there is no period.
             return Play.application().configuration().getString("msg.noTLD");
@@ -183,8 +122,10 @@ public class Application extends Controller {
         TLD toValidate = new TLD();
         toValidate.setDomain(domain.toUpperCase());
 
+        log.debug("Checking the domain.");
         // check if the TLD is in the list.
         if (tlds.contains(toValidate)) {
+            log.debug("Found the domain.");
             // TLD was in list. All is well: return null.
             return null;
         }
@@ -192,6 +133,16 @@ public class Application extends Controller {
         // unable to validate TLD, return error.
         log.info("Unable to find domain: {}", domain);
         return String.format(Play.application().configuration().getString("msg.invalidTLD"), domain.toLowerCase());
+    }
+
+    /**
+     * Builds a list of the addresses in the DB. Used to display addresses in DB to user in index generation.
+     */
+    private List<Address> listAddress() {
+        // Get the addresses from the DB.
+        List<Address> al = addressService.getAllAddresses();
+        log.info("Returning list of stored addresses. Length: {}", al.size());
+        return al;
     }
 }
 
--- app/forms/AddressForm.java
+++ app/forms/AddressForm.java
@@ -1,18 +1,77 @@
 package forms;
 
+import models.TLD;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import play.Play;
 import play.data.validation.Constraints.Required;
+import services.TLDService;
+
+import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Form to ask user for an email address.
  * <p>
  * address must pass play's{@link Required} validations.
  */
-public class AddressForm {
+public class AddressForm{
     // validate that the input is non-empty.
     @Required
     private String address;
+    private final static Logger log = LoggerFactory.getLogger(AddressForm.class);
 
-    public AddressForm() {
+    /**
+     * Validate that a {@link String} is an email address. Used in conjunction with @Required
+     * validation on {@link AddressForm}.
+     * <p>
+     * Needs {@link AddressForm#tlds} to be set for the class and contain a list of valid {@link TLD}s.
+     * This can be set using {@link TLDService#getAllTLDs()}.
+     * <p>
+     * Checks that string passes initial regex (looks roughly like an email).
+     * Checks that the overall length is less than 255.
+     * Checks that the local length is less than 65.
+     * Requires that the String does not contain '..' then splits string on periods.
+     * Requires that result of split is at least length 2.
+     * Then requires that the last substring from the split does not contain '@'.
+     * Finally compares that substring against list of {@link TLD}s. If a match is found the string is accepted.
+     *
+     * @return Null if String is an email, or message detailing why String is not an email.
+     */
+    public String validate() {
+        log.debug("Validating TLD.");
+
+        // verify address meets regex, stolen from Play's email validation, but removed length constraints since they were not helpful
+        // and are enforced below.
+        final Pattern regex = java.util.regex.Pattern.compile("\\b[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)*\\b");
+        if (!regex.matcher(address).matches()) {
+            return Play.application().configuration().getString("msg.addressNotMatch");
+        }
+
+        //split the address on the @
+        String[] splitAddress = address.split("@");
+
+        // check the overall length of the address.
+        if(address.length() > 254){
+            log.debug("Overall address is too long. Length: {}", address.length());
+            return String.format(Play.application().configuration().getString("msg.addressTooLongOverall"), address.length());
+        }
+
+        //check 'local' part for length.
+        if(splitAddress[0].length() > 64){
+            log.debug("Local part of address is too long. Length: {}", splitAddress[0].length());
+            return String.format(Play.application().configuration().getString("msg.addressTooLongLocal"), splitAddress[0], splitAddress[0].length());
+        }
+
+        // Check for double period.
+        if (address.contains("..")) {
+            return Play.application().configuration().getString("msg.doublePeriod");
+        }
+        return null;
+    }
+
+    public AddressForm(){
     }
 
     public String getAddress() {
